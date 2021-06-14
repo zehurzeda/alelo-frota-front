54094677332d853de104176b01aa3609f4a0c788
Jose Ferreira <jose.ferreira@softplan.com.br>
2021-06-14 03:45:12 -0300

    Correção no servidor

--- server.js
+++ server.js
@@ -1,36 +1,14 @@
 //Install express server
-const express = require("express");
-const path = require("path");
-const cors = require('cors');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-// add this code
-const whitelist = ['http://localhost:3000']; // list of allow domain
-
-const corsOptions = {
-  origin: function (origin, callback) {
-      if (!origin) {
-          return callback(null, true);
-      }
-
-      if (whitelist.indexOf(origin) === -1) {
-          var msg = 'The CORS policy for this site does not ' +
-              'allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-  }
-}
-
-// end 
-app.use(cors(corsOptions));
-
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/alelo-frota-test"));
+app.use(express.static('./dist/alelo-frota-test'));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/alelo-frota-test/" })
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/alelo-frota-test/'}),
 );
 
 // Start the app by listening on the default Heroku port
