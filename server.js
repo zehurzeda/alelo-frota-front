9d2f0ae8b144e135004cdf49243b1c4aba7dcdb2
Jose Ferreira <jose.ferreira@softplan.com.br>
2021-06-14 03:06:47 -0300

    Configuração CORS

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 //Install express server
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+const corsOptions = {
+  origin: function (origin, callback) {
+      if (!origin) {
+          return callback(null, true);
+      }
+
+      if (whitelist.indexOf(origin) === -1) {
+          var msg = 'The CORS policy for this site does not ' +
+              'allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+  }
+}
+
+// end 
+app.use(cors(corsOptions));
 
 // Serve only the static files form the dist directory
 app.use(express.static("./dist/alelo-frota-test"));
