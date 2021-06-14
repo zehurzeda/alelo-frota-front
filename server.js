bf753a212cf2c115c35ac6fb4442bc94938e88bb
Jose Ferreira <jose.ferreira@softplan.com.br>
2021-06-14 02:58:03 -0300

    configuracao cors

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/alelo-frota-test'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/alelo-frota-test/'}),
+// Serve only the static files form the dist directory
+app.use(express.static("./dist/alelo-frota-test"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/alelo-frota-test/" })
 );
 
 // Start the app by listening on the default Heroku port
